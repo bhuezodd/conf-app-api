df86dd1e10dfaa3ce5627ba2ea11569fcbb6b48e
Boris Huezo <borisdev@pop-os.localdomain>
2021-07-31 17:50:27 -0600

    Message error

--- app/Http/Controllers/UserControler.php
+++ app/Http/Controllers/UserControler.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -48,9 +49,8 @@ class UserControler extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
-    {
-        
+    public function store(UserRequest $request)
+    { 
         $user = $this->user->create([
             "email" => $request->email,
             "password" => Hash::make($request->password),
