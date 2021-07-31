270d6047cd6b8220eafc1818d1558c9c11814c4d
Boris Huezo <borisdev@pop-os.localdomain>
2021-07-31 16:49:02 -0600

    Add auth

---
+++ app/Http/Controllers/UserControler.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserControler extends Controller
+{
+    protected User $user;
+    public function __construct(User $user)
+    {
+        $this->user = $user; 
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function login(Request $request)
+    {
+        if ($user = $this->user->where('email',$request->email)->first()) {
+            if (!$user || !Hash::check($request->password, $user->password)) {
+                return response()->json(['message' => 'The provided credentials are incorrect'], 401);
+            }
+            $user->tokens()->delete();
+
+            $token = $user->createToken('device_name')->plainTextToken;
+            return $this->respondWithToken($token);
+        }
+        return response()->json(['message' => 'Unauthorized'], 401);
+    }
+
+    
+    protected function respondWithToken($token)
+    {
+        return response()->json(
+            [
+                'token_type' => 'Bearer',
+                'token' => $token,
+            ]
+        );
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        
+        $user = $this->user->create([
+            "email" => $request->email,
+            "password" => Hash::make($request->password),
+            "name" => $request->name,
+        ]);
+        return $user;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserControler;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('/login', [UserControler::class, "login"]);
+Route::post('/register', [UserControler::class, "store"]);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
